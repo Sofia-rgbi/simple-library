6bd6a6a59e8e145e3c631e21b70eb7705bd5352d
Ivan Ivanov <ivan.ivanov@example.com>
2025-10-20 14:28:04 +0300

    make ListAllBooks, IssueBookReader, FindReaderByID

--- main.go
+++ main.go
@@ -22,6 +22,27 @@ func main() {
 
     fmt.Println("\nСписок всех книг в библиотеке:")
     myLibrary.ListAllBooks()
+
+    
+fmt.Println("---Тестируем выдачу книг---")
+//Выдаем книгу 1 читателю 1
+err := myLibrary.IssueBookToReader(1, 1)
+if err != nil {
+	fmt.Println("Ошибка выдачи", err)
+}
+
+//Проверить статус книги после выдачи
+book, _ := myLibrary.FindBookByID(1)
+if book != nil {
+	fmt.Println("Статус книги после выдачи:", book)
+}
+
+//Попытка выдать несуществующую книгу
+err = myLibrary.IssueBookToReader(99, 1)
+if err != nil {
+	fmt.Println("Ожидаемая ошибка:", err)
+}
+
 }
 
 
