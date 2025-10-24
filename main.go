e2e24140da2405060f1ef83427591218f5850cee
Ivan Ivanov <ivan.ivanov@example.com>
2025-10-24 12:08:44 +0300

    Redacted models.go, main.go

--- main.go
+++ main.go
@@ -4,15 +4,17 @@ import "fmt"
 
 func main() {
     fmt.Println("Запуск системы управления библиотекой...")
-    // 1. Создаем экземпляр библиотеки
-    myLibrary := &Library{} //Пустая библиотека готова к работе
+
+    // Создаем экземпляр библиотеки
+    myLibrary := &Library{}
+
     fmt.Println("\n--- Наполняем библиотеку ---")
 
-    //2. Добавляем читателей
+    // Добавляем читателей
     myLibrary.AddReader("Fatima", "Tokazova")
     myLibrary.AddReader("Sofia", "Tedeeva")
 
-    //3.Добавляем книги
+    // Добавляем книги
     myLibrary.AddBook("1984", "Джордж Оруэлл", 1949)
     myLibrary.AddBook("Мастер и Маргарита", "Михаил Булгаков", 1967)
 
@@ -20,59 +22,107 @@ func main() {
     fmt.Println("Количество читателей:", len(myLibrary.Readers))
     fmt.Println("Количество книг:", len(myLibrary.Books))
 
-    fmt.Println("\nСписок всех книг в библиотеке:")
+    fmt.Println("\n--- Список всех книг в библиотеке ---")
+
+    // Сценарий 1: Успешная выдача книги
+    fmt.Println("\n--- Сценарий 1: Успешная выдача книги ---")
+    err := myLibrary.IssueBookToReader(1, 1)
+    if err != nil {
+        fmt.Println("Ошибка выдачи книги:", err)
+    } else {
+        fmt.Println("Книга успешно выдана читателю")
+        
+        // Проверяем статус книги после выдачи
+        book, _ := myLibrary.FindBookByID(1)
+        if book != nil {
+            fmt.Println("Статус книги после выдачи:", book)
+        }
+    }
+
+    // Сценарий 2: Попытка выдать уже выданную книгу
+    fmt.Println("\n--- Сценарий 2: Попытка выдать уже выданную книгу ---")
+    err = myLibrary.IssueBookToReader(1, 2)
+    if err != nil {
+        fmt.Println("Ошибка выдачи книги:", err)
+    } else {
+        fmt.Println("Книга успешно выдана читателю")
+    }
+
+    // Сценарий 3: Попытка выдать книгу несуществующему читателю
+    fmt.Println("\n--- Сценарий 3: Попытка выдать книгу несуществующему читателю ---")
+    err = myLibrary.IssueBookToReader(1, 99)
+    if err != nil {
+        fmt.Println("Ошибка выдачи книги:", err)
+    } else {
+        fmt.Println("Книга успешно выдана читателю")
+    }
+
+    // Сценарий 4: Попытка выдать несуществующую книгу
+    fmt.Println("\n--- Сценарий 4: Попытка выдать несуществующую книгу ---")
+    err = myLibrary.IssueBookToReader(99, 1)
+    if err != nil {
+        fmt.Println("Ошибка выдачи книги:", err)
+    } else {
+        fmt.Println("Книга успешно выдана читателю")
+    }
+
+    // Сценарий 5: Успешный возврат книги
+    fmt.Println("\n--- Сценарий 5: Успешный возврат книги ---")
+    err = myLibrary.ReturnBook(1)
+    if err != nil {
+        fmt.Println("Ошибка возврата книги:", err)
+    } else {
+        fmt.Println("Книга успешно возвращена в библиотеку")
+        
+        // Проверяем статус книги после возврата
+        book, _ := myLibrary.FindBookByID(1)
+        if book != nil {
+            fmt.Println("Статус книги после возврата:", book)
+        }
+    }
+
+    // Сценарий 6: Попытка вернуть книгу, которая уже в библиотеке
+    fmt.Println("\n--- Сценарий 6: Попытка вернуть книгу, которая уже в библиотеке ---")
+    err = myLibrary.ReturnBook(1)
+    if err != nil {
+        fmt.Println("Ошибка возврата книги:", err)
+    } else {
+        fmt.Println("Книга успешно возвращена в библиотеку")
+    }
+
+    // Сценарий 7: Попытка вернуть несуществующую книгу
+    fmt.Println("\n--- Сценарий 7: Попытка вернуть несуществующую книгу ---")
+    err = myLibrary.ReturnBook(99)
+    if err != nil {
+        fmt.Println("Ошибка возврата книги:", err)
+    } else {
+        fmt.Println("Книга успешно возвращена в библиотеку")
+    }
+
+    // Тестирование функции GetPortFromConfig
+    fmt.Println("\n--- Тестирование функции GetPortFromConfig ---")
     
+    // Тест 1: Конфиг без порта
+    config1 := map[string]string{
+        "NOPORT": "2132",
+    }
+    port, err := GetPortFromConfig(config1)
+    if err != nil {
+        fmt.Println("Ошибка чтения порта:", err)
+    } else {
+        fmt.Println("Порт номер:", port)
+    }
 
-    
-fmt.Println("---Тестируем выдачу книг---")
-//Выдаем книгу 1 читателю 1
-err := myLibrary.IssueBookToReader(1, 1)
-if err != nil {
-	fmt.Println("Ошибка выдачи", err)
-}
+    // Тест 2: Конфиг с портом
+    config2 := map[string]string{
+        "PORT": "8080",
+    }
+    port, err = GetPortFromConfig(config2)
+    if err != nil {
+        fmt.Println("Ошибка чтения порта:", err)
+    } else {
+        fmt.Println("Порт номер:", port)
+    }
 
-//Проверить статус книги после выдачи
-book, _ := myLibrary.FindBookByID(1)
-if book != nil {
-	fmt.Println("Статус книги после выдачи:", book)
-}
-
-//Попытка выдать несуществующую книгу
-err = myLibrary.IssueBookToReader(99, 1)
-if err != nil {
-	fmt.Println("Ожидаемая ошибка:", err)
-}
-
-err1 := myLibrary.ReturnBook(1)
-if err1 != nil {
-    fmt.Println("Ошибка при выдаче книги: ", err)
-} else {
-    fmt.Println("Книга возвращена успешно")
-}
-
-err2 := myLibrary.ReturnBook(1)
-if err2 != nil {
-    fmt.Println("Ошибка при выдаче книги: ", err)
-} else {
-    fmt.Println("Книга возвращена успешно")
-}
-
-config := map[string]string {
-    "PORT" : "2123",
-}
-
-
-//config1 := map[string]string {
-//    "NOPORT" : "2132",
-//}
-//
-port, err := GetPortFromConfig(config)
-if err != nil {
-    fmt.Println("Произошла ошибка чтения порта:", err)
-}else {
-    fmt.Println("Порт номер:", port)
-}
-
-
-
+    fmt.Println("\n--- Работа системы управления библиотекой завершена ---")
 }
